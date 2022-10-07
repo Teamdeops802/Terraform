9caaf106d4bbdf4b4ade7da255fe86fa80b78a12
Teamdeops802 <114252857+Teamdeops802@users.noreply.github.com>
2022-10-07 15:54:25 -0500

    Update output.tf

--- modules/infastructure/output.tf
+++ modules/infastructure/output.tf
@@ -9,15 +9,3 @@ output "jenkins_private_ip" {
 output "ansible_private_ip" {
   value = aws_instance.ansible.private_ip
 }
-
-output "k8s_private_ip" {
-  value = aws_instance.k8s.public_ip
-}
-
-output "jenkins_private_ip" {
-  value = aws_instance.jenkins.public_ip
-}
-
-output "ansible_private_ip" {
-  value = aws_instance.ansible.public_ip
-}

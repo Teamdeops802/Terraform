25b7e255b7bc32544b1267b01fb6bb67bb0522af
imasaliev <imasaliev@gmail.com>
2022-10-07 08:38:42 -0500

    changes to  inventory file

--- modules/infastructure/output.tf
+++ modules/infastructure/output.tf
@@ -9,3 +9,15 @@ output "jenkins_private_ip" {
 output "ansible_private_ip" {
   value = aws_instance.ansible.private_ip
 }
+
+output "k8s_private_ip" {
+  value = aws_instance.k8s.public_ip
+}
+
+output "jenkins_private_ip" {
+  value = aws_instance.jenkins.public_ip
+}
+
+output "ansible_private_ip" {
+  value = aws_instance.ansible.public_ip
+}
